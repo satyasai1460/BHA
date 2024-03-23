e09dc8e73efdbc90bf7c4bdc1434d3338eada624
Satya sai Devarapalli <68277833+satyasai1460@users.noreply.github.com>
2024-03-23 17:25:18 +0530

    Update go-lang-tool.sh

--- go-lang-tool.sh
+++ go-lang-tool.sh
@@ -9,7 +9,7 @@ wget https://go.dev/dl/go1.22.1.linux-amd64.tar.gz
 echo ""
 
 #Extracting Go Lang
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.6.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.1.linux-amd64.tar.gz
 
 # Define the Go Lang variables you want to remove
 go_variables=("GOPATH" "GOROOT" "PATH")
